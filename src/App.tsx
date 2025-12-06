5649885525a14c59f64e0c8af96c72bb4f289cfd
Amer <m1o1h1a1a1@gmail.com>
2025-12-06 02:58:37 +0200

    added home page and complete registration for doctor, student doctor and receptionist

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { Route, Routes } from "react-router";
+import PatientRegistration from "./Registration/PatientRegistration";
+import Registration from "./Registration/Registration";
+import DoctorRegistration from "./Registration/DoctorRegistration";
+import StudentRegistration from "./Registration/StudentRegistration";
+import ReceptionistRegistration from "./Registration/ReceptionistRegistration";
+import HomePage from "./HomePage/HomePage";
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-4xl">App Page</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/registration" element={<Registration />}>
+          <Route index element={<PatientRegistration />} />
+          <Route
+            path="patient-registration"
+            element={<PatientRegistration />}
+          />
+          <Route
+            path="doctor-registration"
+            index
+            element={<DoctorRegistration />}
+          />
+          <Route
+            path="student-registration"
+            index
+            element={<StudentRegistration />}
+          />
+          <Route
+            path="receptionist-registration"
+            index
+            element={<ReceptionistRegistration />}
+          />
+        </Route>
+      </Routes>
     </div>
   );
 };
